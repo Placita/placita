1a661ccdcee791769758f288493f4c42952123b7
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-05-22 12:11:57 -0700

    [create] menuItem model.

---
+++ models/menuItem.js
@@ -0,0 +1,36 @@
+// Model for MenuItems
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const menuItemSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    enum: ['APPETIZERS', 'SALADS', 'TACOS', 'ENTREE', 'SIDES'],
+    required: false
+  },
+  description: [{
+    type: String,
+    required: true
+  }],
+  menu: {
+    type: String,
+    enum: ['BRUNCH', 'DINNER', 'DESSERT', 'DRINKS'],
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'Admin',
+    required: false
+  }
+})
+
+module.exports = mongoose.model('MenuItem', menuItemSchema)
