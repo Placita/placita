4b66befcd79241bfb1fb0c9e606007dfd0250f48
Starlight Romero <starlightromero@protonmail.com>
2021-05-22 21:06:15 -0700

    [update] finish hover effects

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,33 @@
 const eat = document.getElementById('eat')
+const drink = document.getElementById('drink')
+const visit = document.getElementById('visit')
 
 eat.addEventListener('mouseenter', () => {
   eat.innerHTML = 'Comer'
-  // eat.style.fontSize = '50px'
-  console.log(eat.style.fontSize)
+  eat.style.fontSize = '50px'
 })
 
 eat.addEventListener('mouseleave', () => {
   eat.innerHTML = 'Eat'
+  eat.style.fontSize = '64px'
+})
+
+drink.addEventListener('mouseenter', () => {
+  drink.innerHTML = 'Beber'
+  drink.style.fontSize = '50px'
+})
+
+drink.addEventListener('mouseleave', () => {
+  drink.innerHTML = 'Drink'
+  drink.style.fontSize = '64px'
+})
+
+visit.addEventListener('mouseenter', () => {
+  visit.innerHTML = 'Visitar'
+  visit.style.fontSize = '50px'
+})
+
+visit.addEventListener('mouseleave', () => {
+  visit.innerHTML = 'Visit'
+  visit.style.fontSize = '64px'
 })
