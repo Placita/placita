acc67fe705e8d947ff3e4dd9f11465de1783c5d2
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-05-22 21:26:15 -0700

    [update] Added transitions to homepage elements.

--- public/js/index.js
+++ public/js/index.js
@@ -3,31 +3,31 @@ const drink = document.getElementById('drink')
 const visit = document.getElementById('visit')
 
 eat.addEventListener('mouseenter', () => {
-  eat.innerHTML = 'Comer'
-  eat.style.fontSize = '50px'
+  setTimeout(() => {
+    eat.innerHTML = 'Comer'
+  }, 215)
 })
 
 eat.addEventListener('mouseleave', () => {
   eat.innerHTML = 'Eat'
-  eat.style.fontSize = '64px'
 })
 
 drink.addEventListener('mouseenter', () => {
-  drink.innerHTML = 'Beber'
-  drink.style.fontSize = '50px'
+  setTimeout(() => {
+    drink.innerHTML = 'Tomar'
+  }, 215)
 })
 
 drink.addEventListener('mouseleave', () => {
   drink.innerHTML = 'Drink'
-  drink.style.fontSize = '64px'
 })
 
 visit.addEventListener('mouseenter', () => {
-  visit.innerHTML = 'Visitar'
-  visit.style.fontSize = '50px'
+  setTimeout(() => {
+    visit.innerHTML = 'Visitar'
+  }, 215)
 })
 
 visit.addEventListener('mouseleave', () => {
   visit.innerHTML = 'Visit'
-  visit.style.fontSize = '64px'
 })
