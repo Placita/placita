1f5264634b9b56638589599e5d374c1d77a0b7d4
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-05-22 20:02:51 -0700

    [update] Create happenings model, test adding and removing events.

--- models/happening.js
+++ models/happening.js
@@ -1,1 +1,26 @@
-// Model for happening
+// Model for happenings
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const happeningsSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'Admin',
+    required: true
+  },
+  dateTime: {
+    type: Date,
+    required: true
+  }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Happenings', happeningsSchema)
