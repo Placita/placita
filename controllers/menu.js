013eac7a613201a151709d1ac253b40b6bd583e8
Starlight Romero <starlightromero@protonmail.com>
2021-05-26 13:01:43 -0700

    [update] menu controllers to use 'menu' template

--- controllers/menu.js
+++ controllers/menu.js
@@ -3,34 +3,34 @@ const MenuItem = require('../models/menuItem')
 
 // Render specific menu pages
 
-exports.getMenus = (req, res) => {
-  res.render('menus/menus')
-}
-
 exports.getBrunchMenu = async (req, res) => {
   const brunchMenu = await MenuItem.find({ menu: 'BRUNCH' }).lean()
-  res.render('menus/brunch', {
+  res.render('menu', {
+    menuTitle: 'Brunch',
     menu: brunchMenu
   })
 }
 
 exports.getDinnerMenu = async (req, res) => {
   const dinnerMenu = await MenuItem.find({ menu: 'DINNER' }).lean()
-  res.render('menus/dinner', {
+  res.render('menu', {
+    menuTitle: 'Dinner',
     menu: dinnerMenu
   })
 }
 
 exports.getDessertMenu = async (req, res) => {
   const dessertMenu = await MenuItem.find({ menu: 'DESSERT' }).lean()
-  res.render('menus/dessert', {
+  res.render('menu', {
+    menuTitle: 'Dessert',
     menu: dessertMenu
   })
 }
 
 exports.getDrinksMenu = async (req, res) => {
   const drinksMenu = await MenuItem.find({ menu: 'DRINKS' }).lean()
-  res.render('menus/drinks', {
+  res.render('menu', {
+    menuTitle: 'Drinks',
     menu: drinksMenu
   })
 }
