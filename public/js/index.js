6356e7e64c18cd9ef46f4def6e3a0ef93b3754f2
Starlight Romero <starlightromero@protonmail.com>
2021-05-27 23:14:45 -0700

    [update] index.js remove setTimeout causing lag

--- public/js/index.js
+++ public/js/index.js
@@ -2,32 +2,32 @@ const eat = document.getElementById('eat')
 const drink = document.getElementById('drink')
 const visit = document.getElementById('visit')
 
-eat.addEventListener('mouseenter', () => {
-  setTimeout(() => {
-    eat.innerHTML = 'Comer'
-  }, 215)
+eat.parentNode.parentNode.addEventListener('mouseenter', () => {
+  eat.innerHTML = 'Comer'
+  eat.style.fontSize = '50px'
 })
 
-eat.addEventListener('mouseleave', () => {
+eat.parentNode.parentNode.addEventListener('mouseleave', () => {
   eat.innerHTML = 'Eat'
+  eat.style.fontSize = '64px'
 })
 
-drink.addEventListener('mouseenter', () => {
-  setTimeout(() => {
-    drink.innerHTML = 'Tomar'
-  }, 215)
+drink.parentNode.parentNode.addEventListener('mouseenter', () => {
+  drink.innerHTML = 'Tomar'
+  drink.style.fontSize = '50px'
 })
 
-drink.addEventListener('mouseleave', () => {
+drink.parentNode.parentNode.addEventListener('mouseleave', () => {
   drink.innerHTML = 'Drink'
+  drink.style.fontSize = '64px'
 })
 
-visit.addEventListener('mouseenter', () => {
-  setTimeout(() => {
-    visit.innerHTML = 'Visitar'
-  }, 215)
+visit.parentNode.parentNode.addEventListener('mouseenter', () => {
+  visit.innerHTML = 'Visitar'
+  visit.style.fontSize = '50px'
 })
 
-visit.addEventListener('mouseleave', () => {
+visit.parentNode.parentNode.addEventListener('mouseleave', () => {
   visit.innerHTML = 'Visit'
+  visit.style.fontSize = '64px'
 })
