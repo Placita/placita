226890a4c7047b945f687f0333d1fc9e5a248ca7
Starlight Romero <starlightromero@protonmail.com>
2021-05-29 21:51:22 -0700

    [update] add add handlebars helper

--- handlebars/helpers.js
+++ handlebars/helpers.js
@@ -2,7 +2,8 @@ const register = (Handlebars) => {
   const helpers = {
     formatDate: (date) => date.toLocaleDateString(),
     formatIngredients: (description) => description.toString().replace(/[,]/g, ' | '),
-    eq: (arg1, arg2) => arg1 === arg2
+    eq: (arg1, arg2) => arg1 === arg2,
+    and: (arg1, arg2) => arg1 && arg2
   }
 
   if (Handlebars && typeof Handlebars.registerHelper === 'function') {
