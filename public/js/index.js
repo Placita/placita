cfcd70d8c9ab4d3af833a756142801c9f3e5779c
Starlight Romero <starlightromero@protonmail.com>
2021-05-29 19:36:54 -0700

    [update] change background image on homepage

--- public/js/index.js
+++ public/js/index.js
@@ -1,33 +1,51 @@
+const main = document.getElementById('main')
 const eat = document.getElementById('eat')
 const drink = document.getElementById('drink')
 const visit = document.getElementById('visit')
 
+const images = [
+  '/images/sample-1.jpg',
+  '/images/sample-2.jpg',
+  '/images/sample-3.jpg',
+  '/images/sample-4.jpg',
+  '/images/sample-5.jpg'
+]
+
+const changeImage = (index) => {
+  main.style.backgroundImage = 'url(' + images[index] + ')'
+}
+
+const startTimer = () => {
+  let index = 0
+  changeImage(index)
+  setInterval(() => {
+    index = index + 1 >= images.length ? 0 : index + 1
+    changeImage(index)
+  }, 5000)
+}
+
+startTimer()
+
 eat.parentNode.parentNode.addEventListener('mouseenter', () => {
   eat.innerHTML = 'Comer'
-  eat.style.fontSize = '50px'
 })
 
 eat.parentNode.parentNode.addEventListener('mouseleave', () => {
   eat.innerHTML = 'Eat'
-  eat.style.fontSize = '64px'
 })
 
 drink.parentNode.parentNode.addEventListener('mouseenter', () => {
   drink.innerHTML = 'Tomar'
-  drink.style.fontSize = '50px'
 })
 
 drink.parentNode.parentNode.addEventListener('mouseleave', () => {
   drink.innerHTML = 'Drink'
-  drink.style.fontSize = '64px'
 })
 
 visit.parentNode.parentNode.addEventListener('mouseenter', () => {
   visit.innerHTML = 'Visitar'
-  visit.style.fontSize = '50px'
 })
 
 visit.parentNode.parentNode.addEventListener('mouseleave', () => {
   visit.innerHTML = 'Visit'
-  visit.style.fontSize = '64px'
 })
